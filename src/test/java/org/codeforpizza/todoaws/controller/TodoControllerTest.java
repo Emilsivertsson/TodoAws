cb5bba7d59c3892940b9c05eb14300349482d0d0
Emilsivertsson <Emil.sivertsson@gmail.com>
2024-02-02 10:56:08 +0100

    try with testcontainer

--- src/test/java/org/codeforpizza/todoaws/controller/TodoControllerTest.java
+++ src/test/java/org/codeforpizza/todoaws/controller/TodoControllerTest.java
@@ -1,105 +1,111 @@
 package org.codeforpizza.todoaws.controller;
 
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.List;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
+import io.restassured.response.Response;
+
+import io.restassured.parsing.Parser;
+import lombok.extern.slf4j.Slf4j;
 import org.codeforpizza.todoaws.models.Todo;
-import org.codeforpizza.todoaws.service.TodoService;
+import org.codeforpizza.todoaws.repository.TodoRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-@SpringBootTest
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodoControllerTest {
 
-    @Mock
-    private TodoService todoService;
+    @LocalServerPort
+    private int port;
 
-    @InjectMocks
-    private TodoController todoController;
+    static MySQLContainer mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.0-debian"));
+
+
+
+    @BeforeAll
+    static void beforeAll() {
+        mySQLContainer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mySQLContainer.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+    }
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost:" + port;
+        todoRepository.deleteAll();
+        List<Todo> todos = List.of(
+                new Todo(1L, "First title", "First description", false),
+                new Todo(2L, "Second title", "Second description", true),
+                new Todo(3L, "Third title", "Third description", false),
+                new Todo(4L, "Fourth title", "Fourth description", true),
+                new Todo(5L, "Fifth title", "Fifth description", false)
+        );
+        todoRepository.saveAll(todos);
+
+    }
 
     @Test
     void getAllTodos() {
-        // Arrange
-        Todo todo1 = new Todo(1L, "Task 1", "Description 1", false);
-        Todo todo2 = new Todo(2L, "Task 2", "Description 2", true);
-        List<Todo> todos = Arrays.asList(todo1, todo2);
 
-        when(todoService.getAllTodos()).thenReturn(ResponseEntity.ok(todos));
+        RestAssured.defaultParser = Parser.JSON;
 
-        // Act
-        ResponseEntity<List<Todo>> responseEntity = todoController.getAllTodos();
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/")
+                .then()
+                .statusCode(400);
 
-        // Assert
-        assertEquals(ResponseEntity.ok(todos), responseEntity);
     }
 
     @Test
     void getOneTodo() {
-        // Arrange
-        Long todoId = 1L;
-        Todo todo = new Todo(todoId, "Task 1", "Description 1", false);
+        Todo todo = new Todo(1L, "First title", "First description", false);
+        log.info(todoRepository.save(todo).toString());
 
-        when(todoService.getOneTodo(todoId)).thenReturn(ResponseEntity.ok(todo));
+        RestAssured.defaultParser = Parser.JSON;
 
-        // Act
-        ResponseEntity<Todo> responseEntity = todoController.getOneTodo(todoId);
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/1")
+                .then()
+                .statusCode(400);
 
-        // Assert
-        assertEquals(ResponseEntity.ok(todo), responseEntity);
     }
 
-    @Test
-    void createTodo() {
-        // Arrange
-        Todo todoToCreate = new Todo("New Task", "New Description", false);
-        when(todoService.createTodo(any(Todo.class))).thenReturn(ResponseEntity.ok(todoToCreate));
-
-        // Act
-        ResponseEntity<Todo> responseEntity = todoController.createTodo(todoToCreate);
-
-        // Assert
-        assertEquals(ResponseEntity.ok(todoToCreate), responseEntity);
-        verify(todoService).createTodo(any(Todo.class));
-    }
-
-    @Test
-    void updateTodo() {
-        // Arrange
-        Long todoId = 1L;
-        Todo updatedTodo = new Todo(todoId, "Updated Task", "Updated Description", true);
-
-        when(todoService.updateTodo(todoId, updatedTodo)).thenReturn(ResponseEntity.ok(updatedTodo));
-
-        // Act
-        ResponseEntity<Todo> responseEntity = todoController.updateTodo(todoId, updatedTodo);
-
-        // Assert
-        assertEquals(ResponseEntity.ok(updatedTodo), responseEntity);
-        verify(todoService).updateTodo(todoId, updatedTodo);
-    }
-
-    @Test
-    void deleteTodo() {
-        // Arrange
-        Long todoId = 1L;
-
-        when(todoService.deleteTodo(todoId)).thenReturn(ResponseEntity.ok("Todo deleted successfully"));
-
-        // Act
-        ResponseEntity<String> responseEntity = todoController.deleteTodo(todoId);
-
-        // Assert
-        assertEquals(ResponseEntity.ok("Todo deleted successfully"), responseEntity);
-        verify(todoService).deleteTodo(todoId);
-    }
 }
