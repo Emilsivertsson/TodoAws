5051c473bc6c8aa9fd9f762944d60692541265bd
Emilsivertsson <Emil.sivertsson@gmail.com>
2024-02-02 11:42:00 +0100

    mockat

--- src/test/java/org/codeforpizza/todoaws/service/TodoServiceTest.java
+++ src/test/java/org/codeforpizza/todoaws/service/TodoServiceTest.java
@@ -1,94 +1,87 @@
 package org.codeforpizza.todoaws.service;
 
-import org.codeforpizza.todoaws.models.Todo;
-import org.codeforpizza.todoaws.repository.TodoRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
-import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
-@SpringBootTest
+import org.codeforpizza.todoaws.models.Todo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ContextConfiguration(classes = {TodoService.class})
+@ExtendWith(SpringExtension.class)
 class TodoServiceTest {
-
     @Autowired
     private TodoService todoService;
 
-    @Mock
-    private TodoRepository todoRepository;
-
-    Todo todo1 = new Todo(1L,"First title", "First description", false);
-    Todo todo2 = new Todo(2L,"Second title", "Second description", true);
-    Todo todo3 = new Todo(3L,"Third title", "Third description", false);
-    Todo todo4 = new Todo(4L,"Fourth title", "Fourth description", true);
-    Todo todo5 = new Todo(5L,"Fifth title", "Fifth description", false);
-
-
-
-    @BeforeEach
-    void setUp() {
-        todoService = Mockito.mock(TodoService.class);
-        todoRepository = Mockito.mock(TodoRepository.class);
+    /**
+     * Method under test: {@link TodoService#getAllTodos()}
+     */
+    @Test
+    void testGetAllTodos() {
+        ResponseEntity<List<Todo>> actualAllTodos = todoService.getAllTodos();
+        assertNull(actualAllTodos.getBody());
+        assertEquals(400, actualAllTodos.getStatusCodeValue());
+        assertTrue(actualAllTodos.getHeaders().isEmpty());
     }
 
+    /**
+     * Method under test: {@link TodoService#getOneTodo(Long)}
+     */
     @Test
-    @DisplayName("1.Get all todos")
-    void getAllTodos() {
-        List<Todo> todos = Arrays.asList(todo1, todo2, todo3, todo4, todo5);
-
-        Mockito.when(todoService.getAllTodos()).thenReturn(ResponseEntity.ok(todos));
-        ResponseEntity<List<Todo>> response = todoService.getAllTodos();
-
-        assertEquals(5, response.getBody().size());
-
-    }
-    @Test
-    @DisplayName("2.Get one todo")
-    void getOneTodo() {
-        when(todoService.getOneTodo(1L)).thenReturn(ResponseEntity.ok(todo1));
-        Todo fromdb = todoService.getOneTodo(1L).getBody();
-
-        assertEquals(todo1.getTitle(), fromdb.getTitle());
+    void testGetOneTodo() {
+        ResponseEntity<Todo> actualOneTodo = todoService.getOneTodo(1L);
+        assertNull(actualOneTodo.getBody());
+        assertEquals(400, actualOneTodo.getStatusCodeValue());
+        assertTrue(actualOneTodo.getHeaders().isEmpty());
     }
 
+    /**
+     * Method under test: {@link TodoService#deleteTodo(Long)}
+     */
     @Test
-    @DisplayName("5.Delete todo")
-    void deleteTodo() {
-        when(todoService.deleteTodo (1L)).thenReturn(ResponseEntity.ok("Todo deleted"));
-        String response = todoService.deleteTodo (1L).getBody();
-
-        assertEquals("Todo deleted", response);
+    void testDeleteTodo() {
+        ResponseEntity<String> actualDeleteTodoResult = todoService.deleteTodo(1L);
+        assertNull(actualDeleteTodoResult.getBody());
+        assertEquals(400, actualDeleteTodoResult.getStatusCodeValue());
+        assertTrue(actualDeleteTodoResult.getHeaders().isEmpty());
     }
 
-
+    /**
+     * Method under test: {@link TodoService#updateTodo(Long, Todo)}
+     */
     @Test
-    @DisplayName("4.Update todo")
-    void updateTodo() {
-        when(todoService.getOneTodo(3L)).thenReturn(ResponseEntity.ok(todo3));
-        Todo todoUpdated = new Todo(3L,"Third title", "Third description", false);
-        when(todoService.updateTodo (3L, todo3)).thenReturn(ResponseEntity.ok(todoUpdated));
-
-        Todo todo = todoService.getOneTodo(3L).getBody();
-        todo.setTitle("Updated title");
-        todoService.updateTodo(3L, todo);
-        Todo todo1 = todoService.getOneTodo(3L).getBody();
-        assertEquals(todo.getTitle(), todo1.getTitle());
+    void testUpdateTodo() {
+        Todo todo = new Todo();
+        todo.setCompleted(true);
+        todo.setDescription("The characteristics of someone or something");
+        todo.setId(1L);
+        todo.setTitle("Dr");
+        ResponseEntity<Todo> actualUpdateTodoResult = todoService.updateTodo(1L, todo);
+        assertNull(actualUpdateTodoResult.getBody());
+        assertEquals(400, actualUpdateTodoResult.getStatusCodeValue());
+        assertTrue(actualUpdateTodoResult.getHeaders().isEmpty());
     }
 
+    /**
+     * Method under test: {@link TodoService#createTodo(Todo)}
+     */
     @Test
-    @DisplayName("3.Create todo")
-    void createTodo() {
-        when(todoService.getOneTodo(6L)).thenReturn(ResponseEntity.ok(todo1));
-        Todo todo = new Todo("First title", "New description", false);
-        todoService.createTodo(todo);
-        Todo todofromdb = todoService.getOneTodo(6L).getBody();
-        assertEquals(todo.getTitle(), todofromdb.getTitle());
+    void testCreateTodo() {
+        Todo todo = new Todo();
+        todo.setCompleted(true);
+        todo.setDescription("The characteristics of someone or something");
+        todo.setId(1L);
+        todo.setTitle("Dr");
+        ResponseEntity<Todo> actualCreateTodoResult = todoService.createTodo(todo);
+        assertNull(actualCreateTodoResult.getBody());
+        assertEquals(400, actualCreateTodoResult.getStatusCodeValue());
+        assertTrue(actualCreateTodoResult.getHeaders().isEmpty());
     }
 }
